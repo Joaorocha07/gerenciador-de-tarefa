1b9c12a3b585ef530246caef5200deee3fdfd300
João Victor <rocha.joao.victor.50@gmail.com>
2024-04-17 16:54:12 -0300

    button sair

--- src/components/menu-side/ListItensMenu.tsx
+++ src/components/menu-side/ListItensMenu.tsx
@@ -2,51 +2,65 @@ import React from 'react'
 
 import { items } from './helper/menu'
 import { useMenu } from '@/hook/useMenu'
-import { Box, List } from '@mui/material'
 import { Home } from '@mui/icons-material'
+import { Box, List, useTheme } from '@mui/material'
 
 import SubMenu from './components/SubMenu'
+import LogoutIcon from '@mui/icons-material/Logout'
 import ListMenuSide from './components/ListMenuSide'
 
 export const ListItensMenu = (): JSX.Element => {
+  const theme = useTheme()
   const { selectedItem, handleItemClick, menuItems, handleSubItemClick } = useMenu(items)
 
   return (
-    <Box flex={1}>
-      <List component="nav">
-        <ListMenuSide
-          text='Dashboard'
-          href='/dashboard'
-          selected={selectedItem === 'dashboard'}
-          onClick={() => { handleItemClick('dashboard') }}
-        >
-          <Home />
-        </ListMenuSide>
+    <>
+      <Box flex={1}>
+        <List component="nav">
+          <ListMenuSide
+            text='Dashboard'
+            href='/dashboard'
+            selected={selectedItem === 'dashboard'}
+            onClick={() => { handleItemClick('dashboard') }}
+          >
+            <Home />
+          </ListMenuSide>
 
-        {menuItems.map((menuItem, index) => (
-          <React.Fragment key={index}>
-            {menuItem.subItems.length > 0 && (
-              <SubMenu
-                text={menuItem.text}
-                selected={selectedItem === menuItem.text.toLowerCase().replace(/\s/g, '')}
-                onClick={() => { handleItemClick(menuItem.text.toLowerCase().replace(/\s/g, '')) }}
-              >
-                {menuItem.subItems.map((subItem, subIndex) => (
-                  <ListMenuSide
-                    key={subIndex}
-                    text={subItem.text}
-                    href={subItem.href}
-                    selected={selectedItem === subItem.text.toLowerCase().replace(/\s/g, '')}
-                    onClick={() => { handleSubItemClick(subItem.text.toLowerCase().replace(/\s/g, '')) }}
-                  >
-                    {subItem.icon}
-                  </ListMenuSide>
-                ))}
-              </SubMenu>
-            )}
-          </React.Fragment>
-        ))}
-      </List>
-    </Box>
+          {menuItems.map((menuItem, index) => (
+            <React.Fragment key={index}>
+              {menuItem.subItems.length > 0 && (
+                <SubMenu
+                  text={menuItem.text}
+                  selected={selectedItem === menuItem.text.toLowerCase().replace(/\s/g, '')}
+                  onClick={() => { handleItemClick(menuItem.text.toLowerCase().replace(/\s/g, '')) }}
+                >
+                  {menuItem.subItems.map((subItem, subIndex) => (
+                    <ListMenuSide
+                      key={subIndex}
+                      text={subItem.text}
+                      href={subItem.href}
+                      selected={selectedItem === subItem.text.toLowerCase().replace(/\s/g, '')}
+                      onClick={() => { handleSubItemClick(subItem.text.toLowerCase().replace(/\s/g, '')) }}
+                    >
+                      {subItem.icon}
+                    </ListMenuSide>
+                  ))}
+                </SubMenu>
+              )}
+            </React.Fragment>
+          ))}
+        </List>
+      </Box>
+      <Box height={theme.spacing(8)}>
+        <ListMenuSide
+          text='Sair'
+          href='/login'
+          selected={selectedItem === 'sair'}
+          onClick={() => { handleItemClick('sair') }}
+        >
+          <LogoutIcon />
+        </ListMenuSide>
+      </Box>
+    </>
   )
 }
