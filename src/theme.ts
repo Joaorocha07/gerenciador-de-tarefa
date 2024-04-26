f3c8e6e0f7ecd7a4947b6da87b3d3f48f51e360e
João Victor <rocha.joao.victor.50@gmail.com>
2024-04-26 14:00:42 -0300

    atualizado sem erros agora

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,25 @@ import { createTheme } from '@mui/material'
 
 const theme = createTheme({
   palette: {
-    mode: 'dark'
+    mode: 'dark',
+    textfield: {
+      border: '#ccc',
+      hoverBorder: '#aaa',
+      focusBorder: '#555',
+      placeholder: '#999',
+      inputText: '#fff'
+    },
+    button: {
+      background: '#007bff',
+      hoverBackground: '#0056b3'
+    },
+    buttonTogglTheme: {
+      background: '#28a745'
+    },
+    iconLogin: {
+      colorSuccess: '#28a745',
+      colorError: '#dc3545'
+    }
   }
 })
 
